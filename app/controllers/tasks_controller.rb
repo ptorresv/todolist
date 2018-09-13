c2761362e83ef36c392d528e639607c70da169ad
ptorresv <pact87@gmail.com>
2018-09-12 23:06:08 -0300

    Se mejora formulario, y se crea vistas para ver tareas y el detalle de la tarea

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,26 @@ class TasksController < ApplicationController
     def index
         @tasks = Task.all
     end
+
+    def new
+        @task = Task.new
+    end
+
+    def show
+        @task = Task.find(params[:id])
+    end
+
+    def create
+
+        @task = Task.create(task_params)
+        if @task.save
+            redirect_to @task
+        else
+            render :new
+        end
+    end
+
+    def task_params
+        params.require(:task).permit(:tarea, :dateStart, :dateStop, :resposable, :estado)
+      end
 end
